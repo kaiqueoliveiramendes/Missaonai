ad1f45e1c7194a611ecea8fd12d127664e7d4f63
kaiqueoliveiramendes <129184576+kaiqueoliveiramendes@users.noreply.github.com>
2024-08-27 08:26:07 -0300

    dsfds

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-const teste =document.querySelector (".caixa-principal");
+const caixaPrincipal = document.querySelector(".caixa-principal")
+const caixaPerguntas = document.querySelector(".caixa-perguntas")
+const caixaAlternativas = document.querySelector(".caixa-alternativas")
+const caixaResultado = document.querySelector(".caixa-resultado")
+const textoResultado = document.querySelector(".texto-resultado")
